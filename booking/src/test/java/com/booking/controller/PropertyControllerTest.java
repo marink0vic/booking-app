8d0b06eecbcf79973de109720764ee26d9671cea
marink0vic <salamanka1985@gmail.com>
2024-04-28 15:12:15 +0200

    add unit tests for property service and controller

---
+++ booking/src/test/java/com/booking/controller/PropertyControllerTest.java
@@ -0,0 +1,117 @@
+package com.booking.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.booking.dto.PropertyDto;
+import com.booking.dto.RoomDto;
+import com.booking.model.Property;
+import com.booking.model.Room;
+import com.booking.service.PropertyService;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@WebMvcTest(PropertyController.class)
+public class PropertyControllerTest {
+
+  private static final Integer PROPERTY_ID = 1;
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private PropertyService propertyService;
+
+  @Test
+  void shouldCreateProperty() throws Exception {
+    PropertyDto property = buildPropertyDto();
+    when(propertyService.saveProperty(any(PropertyDto.class))).thenReturn(property);
+
+    mockMvc.perform(MockMvcRequestBuilders.post("/properties")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(createPropertyRequest()))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+
+    verify(propertyService, times(1)).saveProperty(any(PropertyDto.class));
+  }
+
+  @Test
+  void shouldFindAllProperties() throws Exception {
+    PropertyDto property = buildPropertyDto();
+    when(propertyService.findAllProperties()).thenReturn(List.of(property));
+
+    mockMvc.perform(MockMvcRequestBuilders.get("/properties"))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+        .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").exists());
+
+    verify(propertyService, times(1)).findAllProperties();
+  }
+
+  @Test
+  void shouldFindProperty() throws Exception {
+    PropertyDto property = buildPropertyDto();
+    when(propertyService.findProperty(PROPERTY_ID)).thenReturn(property);
+
+    mockMvc.perform(MockMvcRequestBuilders.get(String.format("/properties/%s",PROPERTY_ID)))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("$.id").exists());
+
+    verify(propertyService, times(1)).findProperty(PROPERTY_ID);
+  }
+
+  @Test
+  void shouldNotFindProperty() throws Exception {
+    when(propertyService.findProperty(PROPERTY_ID)).thenThrow(new EntityNotFoundException());
+
+    mockMvc.perform(MockMvcRequestBuilders.get("/properties/" + PROPERTY_ID))
+        .andExpect(MockMvcResultMatchers.status().isNotFound());
+
+    verify(propertyService, times(1)).findProperty(PROPERTY_ID);
+  }
+
+  private String createPropertyRequest() {
+    return """
+        {
+            "name": "Blue Horizon Apartments",
+            "address": "Sunset Boulevard 34",
+            "contactPhone": "345-433-023",
+            "rooms" : [
+                {
+                    "roomNumber": 601,
+                    "price": 234,
+                    "status": "AVAILABLE"
+                }
+            ]
+        }
+        """;
+  }
+
+  private PropertyDto buildPropertyDto() {
+    return PropertyDto.builder()
+        .id(PROPERTY_ID)
+        .name("Laguna Hotel")
+        .address("Main Lane 22A")
+        .contactPhone("89-45-34")
+        .rooms(List.of(RoomDto.builder()
+            .roomNumber(1)
+            .price(100.00)
+            .status("AVAILABLE")
+            .build()))
+        .build();
+  }
+
+}
---
+++ booking/src/test/java/com/booking/service/PropertyServiceTest.java
@@ -0,0 +1,122 @@
+package com.booking.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import com.booking.converter.PropertyConverter;
+import com.booking.converter.RoomConverter;
+import com.booking.dto.PropertyDto;
+import com.booking.dto.RoomDto;
+import com.booking.model.Property;
+import com.booking.model.Room;
+import com.booking.repository.PropertyRepository;
+import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PropertyServiceTest {
+
+  private static final Integer PROPERTY_ID = 1;
+
+  @Mock
+  private PropertyRepository propertyRepository;
+
+  private final PropertyConverter propertyConverter = Mockito
+      .spy(new PropertyConverter(Mockito.spy(new RoomConverter())));
+
+  @InjectMocks
+  private PropertyService propertyService;
+
+  @Test
+  void shouldCreateProperty() {
+    // Given
+    Property property = buildProperty();
+
+    PropertyDto propertyDto = buildPropertyDto();
+    when(propertyRepository.save(any())).thenReturn(property);
+    // When
+    PropertyDto savedProperty = propertyService.saveProperty(propertyDto);
+    // Then
+    assertNotNull(savedProperty.getId());
+    assertEquals(1, property.getId());
+  }
+
+  @Test
+  void shouldFindAllProperties() {
+    // Given
+    List<Property> properties = List.of(buildProperty());
+    when(propertyRepository.findAll()).thenReturn(properties);
+    // When
+    List<PropertyDto> propertyDtos = propertyService.findAllProperties();
+    // Then
+    assertEquals(properties.size(), propertyDtos.size());
+    for (PropertyDto property : propertyDtos) {
+      assertNotNull(property.getId());
+    }
+  }
+
+  @Test
+  void shouldFindProperty() {
+    // Given
+    Property property = buildProperty();
+    when(propertyRepository.findById(PROPERTY_ID)).thenReturn(Optional.of(property));
+    // When
+    PropertyDto propertyDto = propertyService.findProperty(PROPERTY_ID);
+    // Then
+    assertNotNull(propertyDto);
+    assertEquals(PROPERTY_ID, propertyDto.getId());
+  }
+
+  @Test
+  void shouldNotFindProperty() {
+    // Given
+    Property property = buildProperty();
+    when(propertyRepository.findById(PROPERTY_ID)).thenReturn(Optional.empty());
+    // When
+    Exception exception = assertThrows(EntityNotFoundException.class,
+        () -> propertyService.findProperty(PROPERTY_ID));
+    // Then
+    assertEquals(String.format("Property with id: %s not found", PROPERTY_ID),
+        exception.getMessage());
+  }
+
+  private PropertyDto buildPropertyDto() {
+    return PropertyDto.builder()
+        .name("Laguna Hotel")
+        .address("Main Lane 22A")
+        .contactPhone("89-45-34")
+        .rooms(List.of(RoomDto.builder()
+            .roomNumber(1)
+            .price(100.00)
+            .status("AVAILABLE")
+            .build()))
+        .build();
+  }
+
+  private Property buildProperty() {
+    final Property property = Property.builder()
+        .id(PROPERTY_ID)
+        .name("Laguna Hotel")
+        .address("Main Lane 22A")
+        .contactPhone("89-45-34")
+        .build();
+    property.setRooms(List.of(Room.builder()
+        .property(property)
+        .roomNumber(1)
+        .price(100.00)
+        .status("AVAILABLE")
+        .build()));
+    return property;
+  }
+
+}
